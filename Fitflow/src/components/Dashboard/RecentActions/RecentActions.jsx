cfbd7850f60b98c48f025671977dc8f05b6f717b
Alvin Muira <alvinmuira@gmail.com>
2025-11-26 23:22:16 +0300

    Refactor RecentActions component to fetch and display real workout data

--- Fitflow/src/components/Dashboard/RecentActions/RecentActions.jsx
+++ Fitflow/src/components/Dashboard/RecentActions/RecentActions.jsx
@@ -1,19 +1,16 @@
-import React from "react";
-import "./RecentActions.css"
-
-const sampleData = [
-  { id: 1, title: "Chest Day", date: "24 Nov", duration: "45 min", kcal: 350 },
-  { id: 2, title: "Leg Day", date: "23 Nov", duration: "50 min", kcal: 420 },
-  { id: 3, title: "Back Workout", date: "22 Nov", duration: "40 min", kcal: 300 },
-  { id: 4, title: "Full Body", date: "21 Nov", duration: "60 min", kcal: 500 },
-  { id: 5, title: "Cardio", date: "20 Nov", duration: "30 min", kcal: 220 },
-];
+import React, { useEffect, useState } from "react";
+import "./RecentActions.css";
 
 function RecentActions() {
+  const [workouts, setWorkouts] = useState([]);
+
+  useEffect(() => {
+      {/* fuction on fetching data */}
+}, []);
+
   return (
     <div className="recent-card">
       <h2>Recent Workouts</h2>
- {/* Tabled data whould be passed here*/ }
       <table className="recent-table">
         <thead>
           <tr>
@@ -23,14 +20,13 @@ function RecentActions() {
             <th>Kcal</th>
           </tr>
         </thead>
-
         <tbody>
-          {sampleData.map((item) => (
-            <tr key={item.id}>
-              <td>{item.title}</td>
-              <td>{item.date}</td>
-              <td>{item.duration}</td>
-              <td>{item.kcal}</td>
+          {workouts.map((w) => (
+            <tr key={w.id}>
+              <td>{w.name}</td>
+              <td>{new Date(w.date).toLocaleDateString()}</td>
+              <td>{w.duration} min</td>
+              <td>{w.calories_burned}</td>
             </tr>
           ))}
         </tbody>
